d98c99d332642c0edb6f404bb50c62ce4a8af33f
Tem Taepaisitphongse <temt4510@gmail.com>
2024-08-07 22:57:06 +0700

    Added Listening react component, updated currently info

---
+++ src/components/Listening.tsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+import "../styles/footer-item.scss";
+
+const ListeningTo = () => {
+  useEffect(() => {
+    const fetchMusic = async () => {
+      const response = await fetch(
+        `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=tem_t&api_key=82ffa9b5246e4b2afe2746ab9abda62f&format=json&limit=1`
+      );
+      const data = await response.json();
+      const track = data.recenttracks.track[0];
+      const { name: title, url: link } = track;
+
+      console.log(data);
+
+      let listening;
+      if (track["@attr"] !== undefined) {
+        listening = "Now Listening";
+      } else {
+        const timestamp = parseInt(track.date.uts);
+        const date = new Date(timestamp * 1000);
+        const dateFormat =
+          date.getHours() +
+          ":" +
+          (date.getMinutes() < 10
+            ? "0" + date.getMinutes()
+            : date.getMinutes()) +
+          ", " +
+          date.toDateString();
+        listening = dateFormat;
+      }
+
+      document.getElementById("track-title")!.innerHTML = title;
+      document.getElementById("track-link")!.setAttribute("href", link);
+      document.getElementById("track-listening")!.innerHTML = listening;
+    };
+
+    fetchMusic();
+  }, []);
+
+  return (
+    <div className="text-container">
+      <span className="text">
+        <span>🎵</span>
+        <a
+          id="track-link"
+          href="https://open.spotify.com/user/tem_t"
+          target="_blank"
+          className="details"
+        >
+          <span id="track-title" />
+          <span id="track-listening" />
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default ListeningTo;
